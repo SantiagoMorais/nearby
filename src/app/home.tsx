a9c5711657d6b4f8e90fa6c325dc67efdc08df14
Felipe Santiago Morais <contatofelipesantiago@gmail.com>
2024-12-15 20:33:59 -0300

    chore(env): not use environment variables

--- src/app/home.tsx
+++ src/app/home.tsx
@@ -1,8 +1,26 @@
-import { View } from "react-native";
+import { Alert, View } from "react-native";
+import { api } from "@/services/api";
+import { useEffect, useState } from "react";
 
-const Home = () => (
-  <View style={{ flex: 1 }}>
-  </View>
-);
+const Home = () => {
+  const [categories, setCategories] =
+    useState<{ id: string; name: string }[]>();
+
+  async function fetchCategories() {
+    try {
+      const { data } = await api.get("/categories");
+      setCategories(data);
+    } catch (error) {
+      console.log(error);
+      Alert.alert("Categorias", "Não foi possível carregar as categorias.");
+    }
+  }
+
+  useEffect(() => {
+    fetchCategories();
+  }, []);
+
+  return <View style={{ flex: 1 }}></View>;
+};
 
 export default Home;
--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,6 @@
-import { env } from "@/env/env";
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: `http://${env.IP_ADDRESS}:${env.API_PORT}`,
+  baseURL: "http://192.168.5.227:3333",
   timeout: 700,
 });
